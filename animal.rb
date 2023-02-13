c34afc46c4236ecb276f63e3ad6d00e866d792ec
Ian Okumu <okumu.otsembo@gmail.com>
2023-02-13 12:11:26 +0300

    Halfway through

--- animal.rb
+++ animal.rb
@@ -1,3 +1,32 @@
 # TODO: CREATE CLASS AND INSTANCE METHODS (INCLUDE VARIABLES)
+class Animal
+
+    def initialize(name)
+        puts "#{name} has been created"
+    end
+
+    # play sound
+    def play_sound
+        puts "Playing sound"
+    end
+
+    # move
+    def move
+        play_sound
+        puts "Moving"
+    end
+
+end
+
+# object
+zebra = Animal.new("African Zebra")
+giraffe = Animal.new("African Giraffe")
+
+puts "Zebra: #{zebra}"
+puts "Giraffe: #{giraffe}"
+
+# zebra.play_sound
+giraffe.move
+
 
 # TODO: Use self to access instance variables
