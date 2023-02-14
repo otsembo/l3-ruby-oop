b789d0ee6ec37bd631c312212d04b6c13d2769ce
Ian Okumu <okumu.otsembo@gmail.com>
2023-02-14 12:12:03 +0300

    Part 2 complete

--- animal.rb
+++ animal.rb
@@ -1,12 +1,36 @@
 # TODO: CREATE CLASS AND INSTANCE METHODS (INCLUDE VARIABLES)
 class Animal
 
-    def initialize(name)
+    attr_reader :limbs
+    attr_writer :limbs
+
+    attr_accessor :height, :name, :lifespan
+
+    @lifespan = 24
+
+    def initialize(name, lifespan)
+        @name = name
+        @lifespan = lifespan
+
         puts "#{name} has been created"
+        @species = 'Homo Sapiens Sapiens'
+        @limbs = 0
     end
+
+    # getter method for species
+    def species_name
+        @species
+    end
+
+    # setter method for species
+    def species_name=(value)
+        @species = value
+    end
+
 
     # play sound
     def play_sound
+        @sound = "buzz"
         puts "Playing sound"
     end
 
@@ -19,8 +43,18 @@ class Animal
 end
 
 # object
-zebra = Animal.new("African Zebra")
-giraffe = Animal.new("African Giraffe")
+zebra = Animal.new("African Zebra", 20)
+giraffe = Animal.new("African Giraffe", 25)
+
+puts zebra.lifespan
+
+giraffe.species_name = "Giraffa"
+puts giraffe.species_name
+
+zebra.species_name = "Equus quagga"
+
+giraffe.limbs = 4
+puts giraffe.limbs
 
 puts "Zebra: #{zebra}"
 puts "Giraffe: #{giraffe}"
